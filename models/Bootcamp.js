99dcd1f2923baed1cf6f076d3392567d15cc2291
Fredrik Skogstad <fredrik.skogstad94@gmail.com>
2020-03-25 22:33:07 +0100

    DB functionality - querying , relationships etc.

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -99,8 +99,12 @@ const BootcampSchema = new mongoose.Schema(
             type: Date,
             default: Date.now
         }
-    });
-
+    },
+    {
+        toJSON: { virtuals: true },
+        toObject: { virtuals: true }
+    }
+);
 // Create bootcamp slug from the name 
 // Slug formats the name so that it can be used to make a nicer URL
 BootcampSchema.pre("save", function (next) {
@@ -112,7 +116,7 @@ BootcampSchema.pre("save", function (next) {
 BootcampSchema.pre("save", async function (next) {
     const loc = await geocoder.geocode(this.address);
     this.location = {
-        type: "point",
+        type: "Point",
         coordinates: [loc[0].longitude, loc[0].latitude],
         formattedAddress: loc[0].formattedAddress,
         street: loc[0].streetName,
@@ -126,6 +130,21 @@ BootcampSchema.pre("save", async function (next) {
     // Use location object above instead
     this.address = undefined;
     next();
+});
+
+// Cascade delete courses when a bootcamp is deleted
+BootcampSchema.pre("remove", async function (next) {
+    console.log(`Courses beeing removed from bootcamp ${this._id}`);
+    await this.model("course").deleteMany({ bootcamp: this._id });
+    next();
+});
+
+// Reverse populate with virtuals
+BootcampSchema.virtual("courses", {
+    ref: "Course",
+    localField: "_id",
+    foreignField: "bootcamp",
+    justOne: false
 });
 
 module.exports = mongoose.model('Bootcamp', BootcampSchema);
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const colors = require('colors');
+const fileupload = require("express-fileupload");
 const errorHandler = require("./middleware/error");
 const connectDB = require('./config/db');
 
@@ -13,19 +15,27 @@ connectDB();
 
 // Route files
 const bootcamps = require('./routes/bootcamps');
+const courses = require('./routes/courses');
 
 const app = express();
-app.use(express.json());
 
 // Body Parser
+app.use(express.json());
 
 // Dev logging middleware
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// File uploading
+app.use(fileupload());
+
+// Set static folder
+app.use(express.static(path.join(__dirname, "public")));
+
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
+app.use('/api/v1/courses', courses);
 
 app.use(errorHandler);
 
